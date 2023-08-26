26632bbe7ca5a47e7d7256cf2e434eb8e730573a
Maciej Sienkiewicz <maciej@niczyja.pl>
2023-08-26 18:20:57 +0200

    [wip] placing tiles

--- QwirkleKit/Sources/QwirkleKit/GameElements/Board.swift
+++ QwirkleKit/Sources/QwirkleKit/GameElements/Board.swift
@@ -15,13 +15,30 @@ public struct Board: Sendable {
     private var board: [Tile?] = .init(repeating: nil, count: Board.side * Board.side)
     
     public func tile(at x: Int, _ y: Int) -> Tile? {
-        board[Self.side * y + x]
+        board[idx(x, y)]
+    }
+    
+    public func canPlace(_ tile: Tile, at x: Int, _ y: Int) -> Bool {
+        guard self.tile(at: x, y) == nil else { return false }
+
+        //TODO: najpierw prosta wersja, czyli sprawdzanie pojedynczego klocka czy pasuje do danej lokalizacji
+        //TODO: trzeba się rozejrzeć w poziomie i pionie po planszy. rekrursywnie, bo mogą być kolejne sąsiadujące
+        
+//        let tileBefore = self.tile(at: x - 1, y)
+//        let tileAfter = self.tile(at: x + 1, y)
+//
+//        guard try? Line([tileBefore, tile, tileAfter].compactMap({$0})) != nil else { return false }
+        
+        return true
     }
 
-    //TODO: I think we should only be able to put a Line
-    public mutating func put(tile: Tile, at x: Int, _ y: Int) throws {
+    private mutating func put(tile: Tile, at x: Int, _ y: Int) throws {
         guard self.tile(at: x, y) == nil else { throw QwirkleError.tileAlreadyThere }
-        
-        board[Self.side * y + x] = tile
+
+        board[idx(x, y)] = tile
+    }
+
+    private func idx(_ x: Int, _ y: Int) -> Int {
+        Self.side * y + x
     }
 }
